f4f82972023771bd30b77a2892feaed461092b71
Lionel Kaneza <lionkaneza@gmail.com>
2023-08-15 14:44:26 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "montserrat-black": "Montserrat-Black",
+        "montserrat-bold": "Montserrat-Bold",
+        "montserrat-light": "Montserrat-Light",
+        "montserrat-medium": "Montserrat-Medium",
+        "montserrat-regular": "Montserrat-Regular",
+        "montserrat-semi-bold": "Montserrat-SemiBold",
+
+        "montserrat-alternates-black": "MontserratAlternates-Black",
+        "montserrat-alternates-bold": "MontserratAlternates-Bold",
+        "montserrat-alternates-light": "MontserratAlternates-Light",
+        "montserrat-alternates-medium": "MontserratAlternates-Medium",
+        "montserrat-alternates-regular": "MontserratAlternates-Regular",
+        "montserrat-alternates-semi-bold": "MontserratAlternates-SemiBold",
+
+        "raleway-black": "Raleway-Black",
+        "raleway-bold": "Raleway-Bold",
+        "raleway-light": "Raleway-Light",
+        "raleway-medium": "Raleway-Medium",
+        "raleway-regular": "Raleway-Regular",
+        "raleway-semi-bold": "Raleway-SemiBold",
+
+        "roboto-black": "Roboto-Black",
+        "roboto-bold": "Roboto-Bold",
+        "roboto-light": "Roboto-Light",
+        "roboto-medium": "Roboto-Medium",
+        "roboto-regular": "Roboto-Regular",
+      },
+      colors: {
+        "main-blue": "#001342",
+        "main-light-green": "#8ACC50",
+        "main-dark-green": "#116355",
+      },
+    },
+  },
+  plugins: [],
+};
